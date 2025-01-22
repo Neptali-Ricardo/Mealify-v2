cea92a67744c30f4bbc7a67cbc5f4a5760c6d20c
Neptali Castejón <63590772+Neptali-Ricardo@users.noreply.github.com>
2025-01-22 15:28:02 +0000

    Finalizado el maquetado y lógica de loginRegister: - Implementación de formularios de login y register con funcionalidad de navegación. - Ajustes en 'flux' para verificar valores true/false y permitir redirecciones en login exitoso. - Configuración de navegaciones en el 'navbar'. - Inclusión de nuevos recursos gráficos y estilos relacionados.

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,28 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import "../../styles/index.css";
-import { UserForm } from "../component/userForm.jsx";
-import { LoginForm } from "../component/loginForm.jsx";
 import { Navigate, useNavigate } from "react-router-dom";
 import { Jumbotron } from "../component/jumbotron.jsx";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-	const navigate = useNavigate();
-    const [token, setToken] = useState(localStorage.getItem("token"));
-	const [isLogin, setIsLogin] = useState(true);
-    const [formType, setFormType] = useState("register"); // Controla el formulario que se muestra
-
-	// Redirigir al perfil si está logueado
-    const handleClick = () => {
-        if (token) navigate("/loginRegister");
-    };
-
-	useEffect(() => {
-        console.log("isLogin state changed:", isLogin); // Verifica si el estado cambia correctamente
-    }, [isLogin]);
 
 	return (
         <>
