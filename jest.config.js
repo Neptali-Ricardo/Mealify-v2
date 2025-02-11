07baafcaee3bad27a21ee2f3af682b3fd364e0f5
Neptali Castejón <63590772+Neptali-Ricardo@users.noreply.github.com>
2025-02-11 12:47:18 +0000

    Configuración e instalación inicial de Jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // Opcional: Si tienes una carpeta específica para tus tests
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts']
+};
